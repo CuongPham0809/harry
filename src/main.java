12975b8e2529ac8e908ebd1d3485dd5e095b8c0f
CuongPham0809 <131911288+CuongPham0809@users.noreply.github.com>
2025-02-13 13:10:40 +0700

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,17 @@
+
+import javax.swing.SwingUtilities;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author phamd
+ */
+public class main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(LoginApp::new);
+    }
+}
