859f8a4657d9a66ca6908cf57bfdc933a507afe8
CuongPham0809 <131911288+CuongPham0809@users.noreply.github.com>
2025-02-15 15:17:39 +0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,17 +1,10 @@
 
 import javax.swing.SwingUtilities;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author phamd
- */
 public class main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(LoginApp::new);
+        SwingUtilities.invokeLater(() -> new LoginApp());
+        
     }
 }
